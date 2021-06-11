c8f53b7822c28c36a396e952fe28c760dbe1824f
dianjiu <dianjiu@dianjiu.cc>
2021-06-11 15:37:51 +0800

    优化新增博客页面

--- src/views/blog/edit.vue
+++ src/views/blog/edit.vue
@@ -2,105 +2,13 @@
     <div>
         <div class="container">
             <el-form :model="blogForm" :rules="rules" ref="blogForm" label-width="100px" class="demo-blogForm">
-                <div class="dj-div-inline">
-                    <el-form-item label="是否原创" prop="articleType" class="dj-form-inline">
-                        <el-select v-model="blogForm.articleType" placeholder="请选择文章类型">
-                            <el-option label="原创" value="1"></el-option>
-                            <el-option label="转载" value="0"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="是否置顶" prop="articleGrade" class="dj-form-inline">
-                        <el-select v-model="blogForm.articleGrade" placeholder="请选择推荐等级">
-                            <el-option label="置顶" value="10"></el-option>
-                            <el-option label="推荐" value="6"></el-option>
-                            <el-option label="正常" value="0"></el-option>
-                        </el-select>
-                    </el-form-item><br>
-                    <el-form-item label="开启评论" prop="commentStatus" class="dj-form-inline">
-                        <el-select v-model="blogForm.commentStatus" placeholder="请选择推荐等级">
-                            <el-option label="开启" value="1"></el-option>
-                            <el-option label="关闭" value="0"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="同步社区" prop="delivery" class="dj-form-inline">
-                        <el-switch v-model="blogForm.toEmen"></el-switch>
-                    </el-form-item><br>
-                    <el-form-item label="文章专题" prop="articleSpecial" class="dj-form-inline">
-                        <el-select
-                            v-model="blogForm.articleSpecial"
-                            multiple
-                            filterable
-                            allow-create
-                            default-first-option
-                            placeholder="请选择文章专题">
-                            <el-option
-                                v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="文章分类" prop="articleCategories" class="dj-form-inline">
-                        <el-select
-                            v-model="blogForm.articleCategories"
-                            multiple
-                            filterable
-                            allow-create
-                            default-first-option
-                            placeholder="请选择文章分类">
-                            <el-option
-                                v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item><br>
-                    <el-form-item label="文章标签" prop="articleLabel" class="dj-form-inline">
-                        <el-select
-                            v-model="blogForm.articleLabel"
-                            multiple
-                            filterable
-                            allow-create
-                            default-first-option
-                            placeholder="请选择文章标签">
-                            <el-option
-                                v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="Markdown" prop="usedMD" class="dj-form-inline">
-                        <el-switch v-model="blogForm.usedMD"></el-switch>
-                    </el-form-item>
-                </div>
-                <div class="dj-div-inline">
-                    <el-form-item label="上传配图" prop="name">
-                        <el-upload
-                            class="upload-demo"
-                            action="https://jsonplaceholder.typicode.com/posts/"
-                            :show-file-list="false"
-                            :on-success="handleAvatarSuccess"
-                            :before-upload="beforeAvatarUpload">
-                            <img v-if="imageUrl" :src="imageUrl" class="avatar" style="width: 100%;height: 100%">
-                            <i v-else class="el-icon-upload"></i><br>
-                            <span class="el-upload__text">将配图拖到此处，或<em>点击上传</em></span>
-                        </el-upload>
-                    </el-form-item>
-                </div>
                 <el-form-item label="博客标题" prop="articleTitle">
                     <el-input v-model="blogForm.articleTitle"></el-input>
                 </el-form-item>
-                <el-form-item label="文章摘要" prop="articleSummary">
-                    <el-input type="textarea" v-model="blogForm.articleSummary"></el-input>
-                </el-form-item>
-                <el-form-item label="文章内容" prop="articleText">
-                    <quill-editor class="articleText"
+                <el-form-item label="文章内容" prop="blogText">
+                    <quill-editor class="blogText"
                                   ref="myTextEditor"
-                                  v-model="blogForm.articleText"
+                                  v-model="blogForm.blogText"
                                   :options="editorOption"
                                   @blur="onEditorBlur($event)"
                                   @focus="onEditorFocus($event)"
@@ -110,10 +18,92 @@
                     <!--<quill-editor ref="myTextEditor" v-model="content" :options="editorOption"></quill-editor>-->
                     <!--<el-input type="textarea" v-model="blogForm.desc"></el-input>-->
                 </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm('blogForm')">立即发布</el-button>
-                    <!--<el-button @click="resetForm('blogForm')">重置</el-button>-->
+                <el-form-item label="文章摘要" prop="blogSummary">
+                    <el-input type="textarea" v-model="blogForm.blogSummary" rows="5"></el-input>
                 </el-form-item>
+                <div class="">
+                <el-form-item label="文章配图" prop="blogImgs" class="dj-img-upload">
+                    <el-upload
+                        class="upload-demo"
+                        action="https://jsonplaceholder.typicode.com/posts/"
+                        :show-file-list="false"
+                        :on-success="handleAvatarSuccess"
+                        :before-upload="beforeAvatarUpload">
+                        <img v-if="blogForm.blogImgs" :src="blogForm.blogImgs" class="avatar" style="width: 100%;height: 100%">
+                        <i v-else class="el-icon-upload"></i><br>
+                        <span class="el-upload__text">将配图拖到此处，或<em>点击上传</em></span>
+                    </el-upload>
+                </el-form-item>
+            </div>
+                <div class="">
+                    <el-form-item label="是否原创" prop="blogType" class="dj-form-inline">
+                        <el-select v-model="blogForm.blogType" placeholder="请选择文章类型">
+                            <el-option label="原创" value="1"></el-option>
+                            <el-option label="转载" value="0"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="是否置顶" prop="blogGrade" class="dj-form-inline">
+                        <el-select v-model="blogForm.blogGrade" placeholder="请选择推荐等级">
+                            <el-option label="置顶" value="10"></el-option>
+                            <el-option label="推荐" value="6"></el-option>
+                            <el-option label="正常" value="0"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="开启评论" prop="commentStatus" class="dj-form-inline">
+                        <el-select v-model="blogForm.commentStatus" placeholder="请选择推荐等级">
+                            <el-option label="开启" value="1"></el-option>
+                            <el-option label="关闭" value="0"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </div>
+                <div class="">
+                    <el-form-item label="文章专题" prop="blogTopic" class="dj-form-inline">
+                        <el-select v-model="blogForm.blogTopic" filterable placeholder="请选择文章专题">
+                            <el-option
+                                v-for="item in topicOptions"
+                                :key="item.labelName"
+                                :label="item.labelName"
+                                :value="item.labelUrl">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="文章分类" prop="blogSort" class="dj-form-inline">
+                        <el-select v-model="blogForm.blogSort" filterable placeholder="请选择文章分类">
+                            <el-option
+                                v-for="item in sortOptions"
+                                :key="item.labelName"
+                                :label="item.labelName"
+                                :value="item.labelUrl">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="文章标签" prop="blogTags" class="dj-form-inline">
+                        <el-select
+                            v-model="blogForm.blogTags"
+                            multiple
+                            filterable
+                            allow-create
+                            default-first-option
+                            placeholder="请选择文章标签">
+                            <el-option
+                                v-for="item in tagsOptions"
+                                :key="item.labelName"
+                                :label="item.labelName"
+                                :value="item.labelUrl">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                </div>
+                <div class='dj-div-inline'>
+                    <el-form-item label="同步社区" prop="delivery" class="dj-form-inline">
+                        <el-switch v-model="blogForm.toEmen"></el-switch>
+                    </el-form-item>
+                </div>
+                <div class='dj-div-inline'>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('blogForm')">立即发布</el-button>
+                    </el-form-item>
+                </div>
             </el-form>
         </div>
     </div>
@@ -153,61 +143,98 @@ export default {
                 syntax: true, //语法检测
             },
             blogForm: {
-                articleType:"",
-                articleGrade:"",
+                //博客标题
+                blogTitle:"",
+                //博客正文
+                blogText:"",
+                //博客摘要
+                blogSummary:"",
+                //博客类型
+                blogType:"",
+                //博客分类
+                blogSort:"",
+                //博客标签
+                blogTags:[],
+                //博客专题
+                blogTopic:"",
+                //博客热度
+                blogGrade:"",
+                //博客作者
+                blogAuthor:"",
+                //博客缩略图
+                blogImgs:"",
+                //开启评论
                 commentStatus:"",
+                //同步依梦
                 toEmen:true,
-                articleSpecial:[],
-                articleCategories:[],
-                articleLabel:[],
+                //使用MD
                 usedMD:false,
-                articleTitle:"",
-                articleSummary:"",
-                articleText:"",
             },
             rules: {
-                articleTitle: [
+                blogTitle: [
                     { required: true, message: '请输入文章标题', trigger: 'blur' },
                     { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
                 ],
-                articleSummary: [
+                blogSummary: [
                     { required: true, message: '请输入文章概述', trigger: 'blur' },
                     { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
                 ],
-                articleText: [
+                blogText: [
                     { required: true, message: '请输入文章内容', trigger: 'blur' }
                 ],
-                articleType: [
+                blogType: [
                     { required: true, message: '请选择是否原创', trigger: ['change','blur'] }
                 ],
-                articleGrade: [
+                blogGrade: [
                     { required: true, message: '请选择是否置顶', trigger: ['change','blur'] }
                 ],
                 commentStatus: [
                     { required: true, message: '请选择开启评论', trigger: ['change','blur'] }
                 ],
-                articleSpecial: [
-                    { type: 'array', required: true, message: '请至少选择一个专题', trigger: ['change','blur'] }
+                blogTopic: [
+                    { required: true, message: '请至少选择一个专题', trigger: ['change','blur'] }
                 ],
-                articleCategories: [
-                    { type: 'array', required: true, message: '请至少选择一个分类', trigger: ['change','blur'] }
+                blogSort: [
+                    { required: true, message: '请至少选择一个分类', trigger: ['change','blur'] }
                 ],
-                articleLabel: [
+                blogTags: [
                     { type: 'array', required: true, message: '请至少选择一个标签', trigger: ['change','blur'] }
                 ],
             },
-            options: [{
-                value: 'HTML',
-                label: 'HTML'
+            topicOptions: [{
+                labelName: 'SpringBoot',
+                labelUrl: 'springboot',
             }, {
-                value: 'CSS',
-                label: 'CSS'
+                labelName: 'Spring',
+                labelUrl: 'spring'
             }, {
-                value: 'JavaScript',
-                label: 'JavaScript'
+                labelName: 'Vue',
+                labelUrl: 'vue'
+            }],
+            sortOptions: [{
+                labelName: '前端',
+                labelUrl: 'H5',
+            }, {
+                labelName: '后端',
+                labelUrl: 'web'
+            }, {
+                labelName: '生活',
+                labelUrl: 'life'
+            }],
+            tagsOptions: [{
+                labelName: 'Java',
+                labelUrl: 'Java',
+            },{
+                labelName: 'HTML',
+                labelUrl: 'HTML',
+            }, {
+                labelName: 'CSS',
+                labelUrl: 'CSS'
+            }, {
+                labelName: 'JavaScript',
+                labelUrl: 'JavaScript'
             }],
             value: [],
-            imageUrl: ''
         };
     },
     components: {
@@ -235,7 +262,7 @@ export default {
                         }else{
                             params[key] = this.blogForm[key]
                         }
-                        params.articleAuthor ="dianjiu"
+                        params.blogAuthor ="dianjiu"
                         params.status ="1"
                     })
                     console.log(params,'params')
@@ -280,7 +307,10 @@ export default {
 .dj-form-inline {
     display: inline-block;
 }
-
+.dj-article-textarea{
+    height: 200px;
+    line-height: 200px;
+}
 /*.dj-form-inline input {
     width: 150px;
 }*/
